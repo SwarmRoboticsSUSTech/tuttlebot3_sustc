6c96c65415bbd0e2c56a0515b1260a596c947378
Gaoee <lg16@qq.com>
2018-01-29 18:28:07 +0800

    can move to goal

--- turtlebot3_location/src/location_test.cpp
+++ turtlebot3_location/src/location_test.cpp
@@ -12,16 +12,21 @@ using namespace std;
 #define PI 3.1415926
 
 bool getRobotPose(tf::Stamped<tf::Pose>& robot_pose_odom, tf::TransformListener& tfListener);
+bool tramformToBaseFootprint(tf::Stamped<tf::Pose>& orign_pose, tf::Stamped<tf::Pose>& output_pose_odom, tf::TransformListener& tfListener);
 
-tf::Stamped<tf::Pose> g_goal;
+tf::Stamped<tf::Pose> g_goal_map;
 string g_name_space;
 ros::Publisher g_twist_pub;
+bool start = false;
 
 //odom based
 void getGoal(const geometry_msgs::PoseStamped& goal_pose)
 {
   ROS_INFO("Received goal.");
-  tf::poseStampedMsgToTF(goal_pose, g_goal);
+  tf::poseStampedMsgToTF(goal_pose, g_goal_map);
+  start = true;
+//  string frame = g_goal_map.frame_id_;
+//  ROS_INFO("g_goal_map frame:%s", frame.c_str());
 }
 
 void shutdown(int sig)
@@ -44,6 +49,8 @@ int main(int argc, char **argv)
   g_name_space = nh.getNamespace();
   signal(SIGINT, shutdown);
   tf::TransformListener tfListener(ros::Duration(10));
+  tf::TransformBroadcaster transformBR;
+  tf::Transform transform;
   double k1 = 0.2, k2 = 1;
 
   ros::Publisher twist_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
@@ -51,19 +58,29 @@ int main(int argc, char **argv)
   ros::Subscriber get_goal_sub = nh.subscribe(g_name_space + "/goal", 10, &getGoal);
   geometry_msgs::Twist twist;
   tf::Stamped<tf::Pose> robot_pose_odom;
+  tf::Stamped<tf::Pose> robot_pose_goal;
   double control_linear_vel, control_angular_vel;
+  while(!start)
+  {
+    ros::spinOnce();
+  }
 
-  ros::Rate loop_rate(0.5);
+  ros::Rate loop_rate(1);
   while (ros::ok())
   {
+    transform.setOrigin( g_goal_map.getOrigin() );
+    transform.setRotation( g_goal_map.getRotation() );
+    // send the transform of the frame "map" respect to "goal"(child_frame)
+    transformBR.sendTransform(tf::StampedTransform(transform, ros::Time::now(),
+                                                   "/map",
+                                                   g_name_space.substr(1, g_name_space.length() - 1) + "_goal"));
+
     getRobotPose(robot_pose_odom, tfListener);
+    tramformToBaseFootprint(robot_pose_odom, robot_pose_goal, tfListener);
     geometry_msgs::PoseStamped msg;
-    tf::poseStampedTFToMsg(robot_pose_odom, msg);
+    tf::poseStampedTFToMsg(robot_pose_goal, msg);
     double yaw = tf::getYaw(msg.pose.orientation);
-    tf::Vector3 goal_v = g_goal.getOrigin();
-    tf::Vector3 robot_v = robot_pose_odom.getOrigin();
-    double gx = goal_v.getX();
-    double gy = goal_v.getY();
+    tf::Vector3 robot_v = robot_pose_goal.getOrigin();
     double rx = robot_v.getX();
     double ry = robot_v.getY();
     double ux = cos(yaw);
@@ -74,7 +91,7 @@ int main(int argc, char **argv)
     P << rx, ry;
     U << ux, uy;
     J << cos(PI/2), -sin(PI/2), sin(PI/2), cos(PI/2);
-    ROS_INFO("gx:%f gy:%f rx:%f ry:%f", gx, gy, rx, ry);
+    ROS_INFO("rx:%f, ry:%f", rx, ry);
     ROS_INFO("robot_yaw: %f, ux:%f, uy:%f", yaw, ux, uy);
     control_linear_vel = - k1 * P.transpose() * U;
     control_angular_vel = - k2 * P.transpose() * J * U;
@@ -134,10 +151,55 @@ bool getRobotPose(tf::Stamped<tf::Pose>& robot_pose_odom, tf::TransformListener&
   if (current_time.toSec() - robot_pose_odom.stamp_.toSec() > transform_tolerance)
   {
     ROS_WARN_THROTTLE(1.0,
-                      "Costmap2DROS transform timeout. Current time: %.4f, robot_pose_odom stamp: %.4f, tolerance: %.4f",
+                      "Transform timeout. Current time: %.4f, robot_pose_odom stamp: %.4f, tolerance: %.4f",
                       current_time.toSec(), robot_pose_odom.stamp_.toSec(), transform_tolerance);
     return false;
   }
 //  ROS_INFO("robot_pose_odom.frame_id_: %s", robot_pose_odom.frame_id_.c_str());
+  return true;
+}
+
+bool tramformToBaseFootprint(tf::Stamped<tf::Pose>& orign_pose, tf::Stamped<tf::Pose>& output_pose_odom, tf::TransformListener& tfListener)
+{
+  std::string output_frame = g_name_space.substr(1, g_name_space.length() - 1) + "_goal";
+//  orign_pose.frame_id_ = g_name_space.substr(1, g_name_space.length() - 1) + "_odom";
+  orign_pose.stamp_ = ros::Time();
+  output_pose_odom.setIdentity();
+  ros::Time current_time = ros::Time::now();  // save time for checking tf delay later
+
+  try
+  {
+    tfListener.transformPose(output_frame, orign_pose, output_pose_odom);
+  }
+  catch (tf::LookupException& ex)
+  {
+    ROS_ERROR_THROTTLE(1.0, "No Transform available Error looking up robot pose: %s\n", ex.what());
+    return false;
+  }
+  catch (tf::ConnectivityException& ex)
+  {
+    ROS_ERROR_THROTTLE(1.0, "Connectivity Error looking up robot pose: %s\n", ex.what());
+    return false;
+  }
+  catch (tf::ExtrapolationException& ex)
+  {
+    ROS_ERROR_THROTTLE(1.0, "Extrapolation Error looking up robot pose: %s\n", ex.what());
+    return false;
+  }
+  catch (tf2::InvalidArgumentException& ex)
+  {
+    ROS_ERROR_THROTTLE(1.0, "InvalidArgumentException: %s\n", ex.what());
+    return false;
+  }
+
+  double transform_tolerance = 0.1;
+  // check global_pose timeout
+  if (current_time.toSec() - output_pose_odom.stamp_.toSec() > transform_tolerance)
+  {
+    ROS_WARN_THROTTLE(1.0,
+                      "Transform timeout. Current time: %.4f, global_pose stamp: %.4f, tolerance: %.4f",
+                      current_time.toSec(), output_pose_odom.stamp_.toSec(), transform_tolerance);
+    return false;
+  }
   return true;
 }
